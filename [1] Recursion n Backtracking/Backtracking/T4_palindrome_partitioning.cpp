9c09766dd79804bd651edc94e8c89e8bf15b55c8
Vansh Saini <deeptisaini24@gmail.com>
2026-04-09 23:26:24 +0530

    palindromic partitioning of a string

---
+++ [1] Recursion n Backtracking/Backtracking/T4_palindrome_partitioning.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool is_palindrome(int i, int j, string s) {
+    while(i <= j) {
+        if (s[i] != s[j])
+            return false;
+        i++, j--;
+    }
+    return true;
+}
+
+void solve(int i, vector<string> partition, string s, vector<vector<string>> &partitions) {
+    if (i == s.length()) {
+        partitions.push_back(partition);
+        return;
+    }
+    for (int j = i; j <= s.length(); j++) {
+        if (is_palindrome(i, j, s)) {
+            partition.push_back(s.substr(i, j - i + 1));
+            solve(j + 1, partition, s, partitions);
+            partition.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> partition(string s) {
+    vector<vector<string>> partitions;
+    solve(0, vector<string>(), s, partitions);
+    return partitions;
+}
