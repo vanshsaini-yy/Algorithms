e763fd68b8d6d900a93850b3bd0be88309a408ee
Vansh Saini <deeptisaini24@gmail.com>
2026-04-18 01:56:32 +0530

    added blank lines

--- [2] Greedy/T2_fractional_knapsack.cpp
+++ [2] Greedy/T2_fractional_knapsack.cpp
@@ -16,10 +16,13 @@ int get_max_value(int n, int W, vector<int> weight, vector<int> value) {
     vector<pair<float, int>> value_per_weight(n);
     for (int i = 0; i < n; i++)
         value_per_weight[i] = { (float)value[i] / weight[i], i };
+
     sort(value_per_weight.begin(), value_per_weight.end(), greater<pair<float, int>>());
+
     float total_value = 0.0;
     for (int i = 0; i < n; i++) {
         int idx = value_per_weight[i].second;
+
         if (weight[idx] <= W) {
             total_value += value[idx];
             W -= weight[idx];
@@ -34,11 +37,14 @@ int get_max_value(int n, int W, vector<int> weight, vector<int> value) {
 int main() {
     int n, W;
     cin >> n >> W;
+
     vector<int> weight(n), value(n);
+
     for (int i = 0; i < n; i++)
         cin >> weight[i];
     for (int i = 0; i < n; i++)
         cin >> value[i];
+        
     cout << get_max_value(n, W, weight, value) << endl;
     return 0;
 }
