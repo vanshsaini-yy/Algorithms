5123a5d92dfbe1bad147763929c3d6259deb7bf9
Vansh Saini <deeptisaini24@gmail.com>
2026-04-18 09:47:52 +0530

    docs changes and reformat

--- [1] Recursion n Backtracking/Medium Problems/T8_gen_partitions_I.cpp
+++ [1] Recursion n Backtracking/Medium Problems/T8_gen_partitions_I.cpp
@@ -2,20 +2,24 @@
 using namespace std;
 
 
-// Generate all partitions of number n, where order of elements in partition matters.
-// For example, for n = 3, the partitions are:
-// 1 1 1
-// 1 2
-// 2 1
-// 3
-vector<vector<int>> generate_partitions(int n)
-{
+/**
+ * Generate all partitions of number n, where order of elements in partition matters.
+ * For example, for n = 3, the partitions are:
+ *     1 1 1
+ *     1 2
+ *     2 1
+ *     3
+ */
+
+vector<vector<int>> generate_partitions(int n) {
     if (n == 0)
         return {{}};
 
     vector<vector<int>> partitions;
+
     for (int i = 1; i <= n; i++) {
         auto sub_partitions = generate_partitions(n - i);
+
         for (auto& sub_partition : sub_partitions) {
             sub_partition.push_back(i);
             partitions.push_back(sub_partition);
@@ -24,13 +28,13 @@ vector<vector<int>> generate_partitions(int n)
     return partitions;
 }
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
+
     auto partitions = generate_partitions(n);
-    for (auto partition : partitions)
-    {
+
+    for (auto partition : partitions) {
         for (auto x : partition)
             cout << x << " ";
         cout << endl;
--- [1] Recursion n Backtracking/Medium Problems/T9_gen_partitions_II.cpp
+++ [1] Recursion n Backtracking/Medium Problems/T9_gen_partitions_II.cpp
@@ -2,42 +2,45 @@
 using namespace std;
 
 
-// Generate all partitions of number n, where order of elements in partition does NOT matter.
-// For example, for n = 3, the partitions are:
-// 1 1 1
-// 1 2
-// 3
-void generate_partitions(int curr_num, int curr_sum, vector<int> partition, int n, vector<vector<int>> &partitions)
-{
+/**
+ * Generate all partitions of number n, where order of elements in partition does NOT matter.
+ * For example, for n = 3, the partitions are:
+ *     1 1 1
+ *     1 2
+ *     3
+ * 
+ * Approach: This is simply target sum problem where the set is {1, 2, 3, ..., n} and the target sum is n. But we can use same element multiple times.
+ */
+
+void generate_partitions(int curr_num, int curr_sum, vector<int> partition, int n, vector<vector<int>> &partitions) {
     if (curr_sum > n)
         return;
+
     if (curr_num > n)
         return;
+
     if (curr_sum == n) {
         partitions.push_back(partition);
         return;
     }
+
     partition.push_back(curr_num);
     generate_partitions(curr_num, curr_sum + curr_num, partition, n, partitions);
     partition.pop_back();
     generate_partitions(curr_num + 1, curr_sum, partition, n, partitions);
 }
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
+
     vector<vector<int>> partitions;
     generate_partitions(1, 0, {}, n, partitions);
-    for (auto partition : partitions)
-    {
+
+    for (auto partition : partitions) {
         for (auto x : partition)
             cout << x << " ";
         cout << endl;
     }
     return 0;
 }
-
-// This is simply target sum problem.
-// The set is {1, 2, 3, ..., n} and the target sum is n.
-// But we can use same lelement multiple times.
